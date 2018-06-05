a42fc75b416e455bacd67b90c0cc342c141f244a
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-06-05 13:13:03 +0200

    use tabde ctor, col_types is now a character vector fo the main class

--- R/get_tabde.R
+++ R/get_tabde.R
@@ -1,17 +1,16 @@
-#' Title
+#' Get table description from data.frame
 #'
-#' @param x
+#' @param x a `data.frame`
 #'
-#' @return
+#' @return a `data.frame`  with the colums `col_names` and `col_types`
 #' @export
 #'
 #' @examples
 get_tabde <- function(x){
+  stopifnot(is.data.frame(x))
 
-  data.frame(
+  tabde(
     col_names = names(x),
-    col_types = I(lapply(x, class)),
-    stringsAsFactors = FALSE,
-    row.names = NULL
+    col_types = vapply(x, function(.x) class(.x)[[1]], "")
   )
 }
