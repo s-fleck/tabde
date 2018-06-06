b47a87534dd15d94a012d52e5056e7be5eb0792b
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-06-06 13:31:36 +0200

    doc, added write_tabde

--- R/read_tabde.R
+++ R/read_tabde.R
@@ -1,11 +1,16 @@
-#' Title
+#' Read a table defintion file
 #'
-#' @param file
+#' tabde recommends that you store table designs as semicolon (`;`) separated
+#' csv files with header and without rownames. `read_tabde()` reads such files
+#' and assigns the correct table_design subclasses based on the columns that
+#' are present in `file` (f.e. `table_design_sql`, `table_design_fwf`).
 #'
-#' @return
+#'
+#' @param file `character` scalar. File that contains a table design
+#'
+#' @return `read_tabde()` and friends return a `table_design data.frame`
 #' @export
 #'
-#' @examples
 read_tabde <- function(file){
   res <- utils::read.csv2(
     file,
@@ -30,14 +35,12 @@ read_tabde <- function(file){
 
 
 
-#' Title
+#' `read_table_sql()` and `read_table_fwf()` are more strict convience functions
+#' that fail if `file` does not contain `sql` or `fwf`` specific columns.
 #'
-#' @param file
-#'
-#' @return
+#' @rdname read_tabde
 #' @export
 #'
-#' @examples
 read_tabde_sql <- function(file){
   res <- read_tabde(file)
   stopifnot(is_table_design_sql(res))
@@ -47,16 +50,73 @@ read_tabde_sql <- function(file){
 
 
 
-#' Title
-#'
-#' @param file
-#'
-#' @return
+#' @rdname read_tabde
 #' @export
 #'
-#' @examples
 read_tabde_fwf <- function(file){
   res <- read_tabde(file)
   stopifnot(is_table_design_fwf(res))
   res
 }
+
+
+
+
+#' `write_tabde()` is a wrapper around `write.csv2()` that saves files in the
+#' recommended format.
+#'
+#' @return `write_tabde()` and `use_tabde()` return `file` (invisibly)
+#' @rdname read_tabde
+#' @export
+write_tabde <- function(
+  x,
+  file,
+  overwrite = FALSE
+){
+  stopifnot(is.data.frame(x))
+  stopifnot(is_scalar_logical(overwrite))
+
+  if (!overwrite & file.exists(file)){
+    stop("'", file, "' exists.")
+  }
+
+  utils::write.csv2(x, file = file, row.names = FALSE)
+  invisible(file)
+}
+
+
+
+
+#' `use_tabde()` is designed for use during package development. It generates
+#'   a table design from a `data.frame`, and saves it in `inst/table_design/`.
+#'
+#' @rdname read_tabde
+#'
+use_tabde <- function(
+  x,
+  file = NULL,
+  overwrite = FALSE
+){
+  assert_namespace("rprojroot")
+
+  td <- tabde::get_tabde(x)
+
+
+  if (is.null(file)){
+    file <- rprojroot::find_package_root_file(
+      "inst", "table_design", paste0(deparse(substitute(x)), ".csv")
+    )
+  }
+
+  if (!dir.exists(dirname(file))){
+    dir.create(dirname(file), recursive = TRUE)
+  }
+
+  write_tabde(td, file = file, overwrite = overwrite)
+  message("Saved table design to: ", file)
+
+
+  invisible(file)
+}
+
+
