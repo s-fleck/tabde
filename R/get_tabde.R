55864127c6c328210d27511cfbed3ff2fb0b1023
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-06-11 15:03:20 +0200

    get_tabde and use_tabde now support creation of *_sql and *_fwf dummy columns

--- R/get_tabde.R
+++ R/get_tabde.R
@@ -1,5 +1,9 @@
 #' Get table description from data.frame
 #'
+#' `get_tabde()` generates a table design from a `data.frame`. `get_tabde_sql()`
+#' and `get_tabde_fwf()` do the same, but also add the respective dummy columns
+#' for `table_design_sql` and `table_design_fwf` Objects.
+#'
 #' @param x a `data.frame`
 #'
 #' @return a `data.frame` with class [table_design]
@@ -8,12 +12,41 @@
 #' @examples
 #'
 #' get_tabde(iris)
-#'
+#' get_tabde_sql(iris)
+#' get_tabde_fwf(iris)
 get_tabde <- function(x){
   stopifnot(is.data.frame(x))
 
   tabde(
     col_name = names(x),
     col_type = vapply(x, function(.x) class(.x)[[1]], "")
+  )
+}
+
+
+
+
+#' @rdname get_tabde
+get_tabde_sql <- function(x){
+  stopifnot(is.data.frame(x))
+
+  tabde_sql(
+    col_name = names(x),
+    col_type = vapply(x, function(.x) class(.x)[[1]], "")
+  )
+}
+
+
+
+
+#' @rdname get_tabde
+get_tabde_fwf <- function(x){
+  stopifnot(is.data.frame(x))
+
+  tabde_fwf(
+    col_name = names(x),
+    col_type = vapply(x, function(.x) class(.x)[[1]], ""),
+    fwf_start = rep(NA_integer_, ncol(x)),
+    fwf_end   = rep(NA_integer_, ncol(x))
   )
 }
---
+++ R/use_tabde.R
@@ -0,0 +1,97 @@
+#' Create table design file
+#'
+#' `use_tabde()` is designed for use during package development.
+#' If `x` is a `table_design()` it saves it directly to `inst/table_design/`.
+#' If `x` is a normal `data.frame` it converts it to a `table_design`
+#' via [get_tabde()] and then saves it.
+#'
+#' `use_tabde_fwf()` and `use_tabde_sql()` are convenince functions that do the
+#' same, but in addition add the respective dummy columns.
+#'
+#' @rdname use_tabde
+#' @export
+#'
+use_tabde <- function(
+  x,
+  file = if (assert_namespace("rprojroot")) rprojroot::find_package_root_file(
+    "inst", "table_design", paste0(deparse(substitute(x)), ".csv")
+  ),
+  overwrite = FALSE
+){
+  use_tabde_internal(
+    x = x,
+    file = file,
+    overwrite = overwrite,
+    fun = get_tabde
+  )
+}
+
+
+
+
+#' @rdname use_tabde
+#' @export
+use_tabde_fwf<- function(
+  x,
+  file = if (assert_namespace("rprojroot")) rprojroot::find_package_root_file(
+    "inst", "table_design", paste0(deparse(substitute(x)), ".csv")
+  ),
+  overwrite = FALSE
+){
+  use_tabde_internal(
+    x = x,
+    file = file,
+    overwrite = overwrite,
+    fun = get_tabde_sql
+  )
+}
+
+
+
+
+#' @rdname use_tabde
+#' @export
+use_tabde_sql <- function(
+  x,
+  file = if (assert_namespace("rprojroot")) rprojroot::find_package_root_file(
+    "inst", "table_design", paste0(deparse(substitute(x)), ".csv")
+  ),
+  overwrite = FALSE
+){
+  use_tabde_internal(
+    x = x,
+    file = file,
+    overwrite = overwrite,
+    fun = get_tabde_sql
+  )
+}
+
+
+
+
+use_tabde_internal <- function(
+  x,
+  file,
+  overwrite,
+  fun
+){
+  assert_namespace("rprojroot")
+
+
+  if (!is_table_design(x)){
+    td <- fun(x)
+  } else {
+    td <- x
+  }
+
+
+  if (!dir.exists(dirname(file))){
+    dir.create(dirname(file), recursive = TRUE)
+  }
+
+  write_tabde(td, file = file, overwrite = overwrite)
+  message("Saved table design to: ", file)
+
+
+  invisible(file)
+}
