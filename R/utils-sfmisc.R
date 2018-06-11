faa7490d2aef5b29596f1f07d310f10a43cf4965
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-06-11 15:03:49 +0200

    updated sfmisc

--- R/utils-sfmisc.R
+++ R/utils-sfmisc.R
@@ -1,4 +1,4 @@
-# sfmisc utils 0.0.1.9000
+# sfmisc utils 0.0.1.9002
 
 
 
@@ -27,10 +27,75 @@ walk <- function(.x, .f, ...){
 
 assert_namespace <- function(x){
   stopifnot(requireNamespace(x, quietly = TRUE))
+  invisible(TRUE)
 }
 
 
 
+
+
+# conditions --------------------------------------------------------------
+
+#' Condition constructor
+#'
+#' A constructur function for conditions, taken from
+#' \url{http://adv-r.had.co.nz/beyond-exception-handling.html}
+#'
+#' @param subclass Subclass to assign to the condition
+#' @param message  message to be passed to the condition
+#' @param call     call passed on to the conditon
+#' @param ...      further list elements to be passed on to the resulting object
+#'
+#' @return a condition object
+#' @noRd
+#'
+#' @examples
+#'
+#' \dontrun{
+#' # Construct a custom condition
+#' malformed_log_entry_error <- function(text) {
+#'   msg <- paste0("Malformed log entry: ", text)
+#'   condition(
+#'     c("malformed_log_entry_entry", "error"),
+#'     message = msg,
+#'     text = text
+#'   )
+#' }
+#'
+#'
+#' # Signal the condition
+#' parse_log_entry <- function(text) {
+#'   if (!well_formed_log_entry(text)) {
+#'     stop(malformed_log_entry_error(text))
+#'    }
+#' }
+#'
+#'
+#' # Handle the condition
+#' tryCatch(
+#'   malformed_log_entry = function(e) NULL,
+#'   parse_log_entry(text)
+#' )
+#' }
+#'
+condition <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = c(subclass, "condition"),
+    list(message = message, call = call, ...)
+  )
+}
+
+
+
+
+#' @export
+#' @rdname condition
+error <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = c(subclass, "error", 'condition'),
+    list(message = message, call = call, ...)
+  )
+}
 
 # predicates --------------------------------------------------------------
 
