f3f7c499ee72bfab80e4a072c090718e4345a030
Stefan Fleck <stefan.fleck@statistik.gv.at>
2018-08-21 10:18:23 +0200

    `td_fread()` wrapps `data.table::fread()` and supports table_designs

---
+++ R/td_fread.R
@@ -0,0 +1,28 @@
+#' Read csv files fast using the data.table package and table_designs
+#'
+#' A wrapper around [data.table::fread()] that automatically fills the
+#' `col.names` and `colClasses` paramters from a table design.
+#'
+#' @inheritParams data.table::fread
+#' @param table_design
+#' @param ... passed on to [data.table::fread()]
+#' @inherit data.table::fread return
+#'
+#' @export
+#'
+td_fread <- function(
+  input = "",
+  file,
+  table_design,
+  ...
+){
+  assert_namespace("data.table")
+
+  data.table::fread(
+    input,
+    file,
+    col.names = table_design$col_name,
+    colClasses = table_design$col_type,
+    ...
+  )
+}
